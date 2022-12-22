747c9860b139a730818fe2243218468034f8d18e
lonskyne <lonskyne@gmail.com>
2022-12-22 01:20:40 +0100

    Started the SvelteKit project, added demo html output

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<nav>
+	<h1>lons-FM</h1>
+</nav>
+
+<script>
+	let filename = "demo.png"
+	let lastmod = "1/1/1900"
+</script>
+
+<body>
+	<p><b>File name:</b> {filename}</p>
+	<p><b>Last modified:</b> {lastmod}</p>
+	<button>Next</button>
+</body>
